a60ccde78da143bd1a0a9f33b8bcebb68b68a223
Fatumayattani <fyattani@gmail.com>
2025-07-10 09:34:52 +0300

    save changes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import HowItWorks from '../components/HowItWorks';
-import ForSupporters from '../components/ForSupporters';
 import Footer from '../components/Footer';
 
 interface HomePageProps {
@@ -15,7 +14,6 @@ const HomePage: React.FC<HomePageProps> = ({ setShowWalletOptions }) => {
       <Hero setShowWalletOptions={setShowWalletOptions} />
       <Features />
       <HowItWorks />
-      <ForSupporters />
       <Footer />
     </>
   );
